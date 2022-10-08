92cf468c152580cd614a1717c34e098b2bdaf7bd
Jarle Friestad <jarle.friestad@gmail.com>
2022-10-09 00:13:02 +0200

    Upgraded dependencies + fixed integration test

--- specs/integration.spec.ts
+++ specs/integration.spec.ts
@@ -1,15 +1,38 @@
 import fs from "fs";
 
+const getFiles = (path, extension) => {
+  const files = fs.readdirSync(path);
+  return files.filter((file) =>
+    file.match(new RegExp(`.*\.(${extension})`, "ig"))
+  );
+};
+
 describe("vite - build", () => {
   test("html contain preload links", async () => {
-    const htmlFile = "examples/react-demo/dist/index.html";
+    const dist = "examples/react-demo/dist";
+    const htmlFile = `${dist}/index.html`;
+    const assetsDirectory = `${dist}/assets`;
 
     expect(fs.existsSync(htmlFile));
+    expect(fs.existsSync(assetsDirectory));
+
     const htmlContent = fs.readFileSync(htmlFile, {
       encoding: "utf8",
       flag: "r",
     });
 
-    expect(htmlContent).toMatchSnapshot("index_with_preloads");
+    const jsChunks = getFiles(assetsDirectory, "js");
+    expect(jsChunks.length).toBeGreaterThan(0);
+    for (const jsChunk of jsChunks) {
+      expect(htmlContent).toContain(`"/assets/${jsChunk}"`);
+    }
+
+    const cssChunks = getFiles(assetsDirectory, "css");
+    expect(cssChunks.length).toBeGreaterThan(0);
+    for (const cssChunk of cssChunks) {
+      expect(htmlContent).toContain(
+        `<link rel="stylesheet" href="/assets/${cssChunk}" />`
+      );
+    }
   });
 });
