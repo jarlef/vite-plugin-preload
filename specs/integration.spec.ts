e364c5ad0a5a9b2d9ba35c0a2f6c7480c57042a1
Jarle Friestad <jarle.friestad@gmail.com>
2022-10-11 00:19:10 +0200

    Made tests more explicit

--- specs/integration.spec.ts
+++ specs/integration.spec.ts
@@ -1,5 +1,19 @@
 import fs from "fs";
-import { test, describe, expect } from "vitest";
+import { it, describe, expect } from "vitest";
+import { JSDOM } from "jsdom";
+
+const dist = "examples/react-demo/dist";
+const htmlFile = `${dist}/index.html`;
+const assetsDirectory = `${dist}/assets`;
+
+export const getDom = (): JSDOM => {
+  const htmlContent = fs.readFileSync(htmlFile, {
+    encoding: "utf8",
+    flag: "r",
+  });
+
+  return new JSDOM(htmlContent);
+};
 
 const getFiles = (path, extension) => {
   const files = fs.readdirSync(path);
@@ -8,32 +22,58 @@ const getFiles = (path, extension) => {
   );
 };
 
-describe("vite - build", () => {
-  test("html contain preload links", async () => {
-    const dist = "examples/react-demo/dist";
-    const htmlFile = `${dist}/index.html`;
-    const assetsDirectory = `${dist}/assets`;
-
+describe("vite-plugin-preload", () => {
+  it("html asset exists", async () => {
     expect(fs.existsSync(htmlFile));
-    expect(fs.existsSync(assetsDirectory));
+  });
 
-    const htmlContent = fs.readFileSync(htmlFile, {
-      encoding: "utf8",
-      flag: "r",
-    });
+  it("assets directory exists", async () => {
+    expect(fs.existsSync(assetsDirectory));
+  });
+
+  it("html contain main js module", async () => {
+    const dom = getDom();
+    const mainScriptMatch = dom.window.document.querySelector(
+      'script[type="module"]'
+    );
+    expect(mainScriptMatch).toBeTruthy();
+    const ref = mainScriptMatch?.getAttribute("src");
+    const jsChunks = getFiles(assetsDirectory, "js");
+    const jsRefs = jsChunks.map((c) => `/assets/${c}`);
+    expect(jsRefs).toContain(ref);
+  });
+
+  it("html contains preload js modules", async () => {
+    const dom = getDom();
+    const scriptPreloads = dom.window.document.querySelectorAll(
+      'link[rel="modulepreload"]'
+    );
+
+    const scriptRefs = Array.from(scriptPreloads.values()).map((x) =>
+      x.getAttribute("href")
+    );
 
     const jsChunks = getFiles(assetsDirectory, "js");
-    expect(jsChunks.length).toBeGreaterThan(0);
-    for (const jsChunk of jsChunks) {
-      expect(htmlContent).toContain(`"/assets/${jsChunk}"`);
-    }
+    const jsRefs = jsChunks.map((c) => `/assets/${c}`);
+
+    expect(scriptRefs.length).equals(jsRefs.length - 1);
+    scriptRefs.forEach((r) => expect(jsRefs).contains(r));
+  });
+
+  it("html contains css references", async () => {
+    const dom = getDom();
+    const scriptPreloads = dom.window.document.querySelectorAll(
+      'link[rel="stylesheet"]'
+    );
+
+    const stylesheetRefs = Array.from(scriptPreloads.values()).map((x) =>
+      x.getAttribute("href")
+    );
 
     const cssChunks = getFiles(assetsDirectory, "css");
-    expect(cssChunks.length).toBeGreaterThan(0);
-    for (const cssChunk of cssChunks) {
-      expect(htmlContent).toContain(
-        `<link rel="stylesheet" href="/assets/${cssChunk}" />`
-      );
-    }
+    const cssRefs = cssChunks.map((c) => `/assets/${c}`);
+
+    expect(stylesheetRefs.length).equals(cssRefs.length);
+    stylesheetRefs.forEach((r) => expect(cssRefs).contains(r));
   });
 });
