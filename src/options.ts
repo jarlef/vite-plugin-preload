46d89437573dd0666c4ed0db1f21f2c812366fc5
Ben Grynhaus <bengr6@gmail.com>
2023-03-29 20:09:57 +0000

    add support for skipping and configuring prettier formatting

--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,22 @@
+import type { Options as PrettierOptions } from "prettier";
+
 export interface PreloadOptions {
+  /**
+   * @default true
+   */
   includeJs: boolean;
+  /**
+   * @default true
+   */
   includeCss: boolean;
+  /**
+   * @default true
+   */
+  format?: boolean | Omit<PrettierOptions, "parser">;
 }
 
 export const defaultOptions: PreloadOptions = {
   includeJs: true,
   includeCss: true,
+  format: true,
 };
