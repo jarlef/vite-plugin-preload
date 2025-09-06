d99c08a6b2574afddf35aaee5e605d8dfb793ecf
Jarle Friestad <jarle.friestad@gmail.com>
2025-09-06 17:48:40 +0200

    Bump packages

--- examples/react-demo/vite.config.ts
+++ examples/react-demo/vite.config.ts
@@ -5,4 +5,7 @@ import preload from "vite-plugin-preload";
 export default defineConfig({
   plugins: [react(), preload()],
   base: "http://www.example.com",
+  build: {
+    manifest: true
+  }
 });
